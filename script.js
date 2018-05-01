1db6088bbab8415755ac472747d4cae01baaa1a5
Sashin Exists <myself@sashinexists.com>
2018-05-01 16:55:07 +1000

    Organism Class and ES6 features added
    New organisms added

--- script.js
+++ script.js
@@ -1,52 +1,87 @@
-"use strict";
+class Organism {
+    constructor(strategy, size) {
+        this.strategy = strategy;
+        this.size = size;
+    }
+}
 
 function init(strategy1, strategy2) {
-    const strategies = {
-        titForTat: function(history, enemyHistory) {
-                if(history.length===0){return true} 
-                return enemyHistory[enemyHistory.length-1]
-            },
-        alwaysCooperate: function() {return true},
-        alwaysAggress: function() {return false},
-        holdGrudge: function(history, enemyHistory) {
-            if(history.length===0) {
-                return true;
-            }
-            return enemyHistory.every(function(action){
-                return action;
-            });
-        },
-        random: function() {return !Math.round(Math.random())}
+    const populations = {
+        titForTat: new Organism((history, enemyHistory) => {return history.length===0 ? true : enemyHistory[enemyHistory.length-1]}, 10),
+        alwaysCooperate: new Organism(()=>true, 10),
+        //neverCooperate: new Organism(()=>false, 10),
+        holdGrudge: new Organism((history, enemyHistory)=>history.length === 0 ? true : !enemyHistory.some(action => !action), 10),
+        titForDoubleTat: new Organism((history, enemyHistory)=> history.length === 0 ? true : enemyHistory[enemyHistory.length-1] || enemyHistory[enemyHistory.length-2], 10),
+        //titForTripleTat: new Organism((history, enemyHistory)=> history.length === 0 ? true : enemyHistory[enemyHistory.length-1] || enemyHistory[enemyHistory.length-2] || enemyHistory[enemyHistory.length-3], 10),
+        karmaIsABitch: new Organism((history, enemyHistory) => history.length === 0 ? true : probability(enemyHistory.filter(action=>action).length/enemyHistory.length), 10),
+        tatForTit: new Organism((history, enemyHistory) => {return history.length===0 ? true : !enemyHistory[enemyHistory.length-1]}, 10),
+        random: new Organism(()=>!!Math.round(Math.random()), 10),
+        //tenPercent: new Organism(()=>probability(0.1), 10)
     }
-    //Vs Random
-    /*
-    for (var strategy in strategies) {
-        console.log(strategy+": "+play(strategies[strategy], strategies["random"])[0]);
-    }*/
-    for (var player in strategies) {
-        let score = 0;
-        for (var rival in strategies) {
-            score+= play(strategies[player], strategies[rival])[0];
-        }
-        console.log(player+": "+score);
-    }
-    //return play(strategies[strategy1], strategies[strategy2]);
+
+    //console.log("Against a random strategy, the best strategy is "+vsRandom(populations));
+    console.log(`Against the population, the best strategy is ${vsPopulation(populations)}`);
+    
     return "There you go...";
 }
+
+function bestBet(history, enemyHistory) {
+
+}
+
+function vsRandom(populations) {
+    let highestScore = 0;
+    let bestStrategy;
+    for (var population in populations) {
+        let {strategy, size} = populations[population];
+        let score = 0;
+        for (let i = 0; i<size; i++) {
+            score += play(strategy, populations["random"].strategy)[0];           
+        }
+        console.log(population+": "+score);
+        bestStrategy = score > highestScore ? population : bestStrategy;
+        highestScore = bestStrategy === population ? score : highestScore; 
+    }
+    return bestStrategy;
+}
+
+function vsPopulation(populations) {
+    let highestScore = 0;
+    let bestStrategy;
+    for (var player in populations) {
+        let score = 0;
+        let {size} = populations[player];
+        for (let i = 0; i<size; i++) {
+            for (let rival in populations) {
+                let rivalSize = populations[rival].size;
+                for (let j = 0; j<rivalSize; j++) {
+                    score+= play(populations[player].strategy, populations[rival].strategy)[0];
+                }
+            }
+        }
+        console.log(`${player}: ${score}`);
+        bestStrategy = score > highestScore ? player : bestStrategy;
+        highestScore = bestStrategy === player ? score : highestScore;
+    }
+    return bestStrategy;
+}
+
 
 function play(strategy1, strategy2) {
     const history1 = [];
     const history2 = [];
-    const turns = 1000; //change this to Math.random()*100
+    const turns = Math.random()*100;
     let score1 = 0;
     let score2 = 0;
     const cheat = 10;
-    const cheated = -5;
+    const cheated = -10;
     const cooperate = 5;
-    const compete = 0;
+    const compete = -5;
     for (var i = 0; i<turns; i++) {
         let action1 = strategy1(history1, history2);
         let action2 = strategy2(history2, history1);
+        action1 = probability(.95)?action1:!action1;
+        action2 = probability(.95)?action2:!action2;
         history1.push(action1);
         history2.push(action2);
         if(action1&&action2) {
@@ -66,5 +101,10 @@ function play(strategy1, strategy2) {
     }
     return [score1, score2];
 }
+
+function probability(chance) {
+    return Math.random()<chance;
+}
+
 
 init();
